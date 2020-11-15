8c1d3d0e10be47ea466ffbbe9d943359c32e601b
Павел <pashanoy@gmail.com>
2020-11-15 13:39:51 +0200

    Add error handling

--- lab1/token/tokenizer.py
+++ lab1/token/tokenizer.py
@@ -32,6 +32,26 @@ def check_newline(to_check):
 
     return False
 
+def check_parentesis(to_check, counters):
+    """function to track parentesis"""
+    if to_check == "(":
+        counters["round"] += 1
+
+    elif to_check == ")":
+        counters["round"] -= 1
+
+    elif to_check == "[":
+        counters["square"] += 1
+
+    elif to_check == "]":
+        counters["square"] -= 1
+
+    elif to_check == "{":
+        counters["square"] += 1
+
+    elif to_check == "}":
+        counters["square"] -= 1
+
 def tokenize(file_str):
     """
     tokenizer function
@@ -43,8 +63,10 @@ def tokenize(file_str):
     """
 
     res_tokens = []
+    errors = []
 
     position = [1, 1]
+    counters = {"round": 0, "square": 0, "braces": 0}
 
     while len(file_str) > 0:
         i = 0
@@ -54,9 +76,8 @@ def tokenize(file_str):
         while True:         #token extraction logic, ends when token is found or whitespace cleared
             i += 1
             str_to_take = file_str[:i]
-            print(file_str)
 
-            if len(str_to_take) < 2:        #next check are irrelevant if len(str_to_take) >= 2
+            if len(str_to_take) < 2:        #next checks are irrelevant if len(str_to_take) >= 2
                 if check_whitespace(str_to_take):       #if whitespace, skip it
                     file_str = file_str[i:]
                     position[1] += 1
@@ -71,13 +92,17 @@ def tokenize(file_str):
             res = detect_token(str_to_take)
 
             if len(res["true"]) > 1:
-                raise Exception("More than 1 full token match")
+                errors.append(f"Unidentified token at ({position[0]}, {position[1]})")
+                file_str = file_str[len(str_to_take):]              #discard unidentified token
+                break
+
 
             elif len(res["true"]) == 1:
                 if len(res["none"]) == 0:       #case - 1 full token match, no possible other matches
                     res_tokens.append(Token(res["true"][0], str_to_take, tuple(position)))
                     position[1] += len(str_to_take)
                     file_str = file_str[len(str_to_take):]
+                    check_parentesis(str_to_take, counters)
                     break
 
                 else:           #case - 1 full token match, but may match another token, if scanned more
@@ -94,8 +119,16 @@ def tokenize(file_str):
                         res_tokens.append(Token(found_true, found_true_str, tuple(position)))
                         position[1] += len(found_true_str)
                         file_str = file_str[len(found_true_str):]
+                        check_parentesis(found_true_str, counters)
                         break
 
-                    raise Exception("Unidentified token: ", str_to_take)
+                    errors.append(f"Unidentified token at ({position[0]}, {position[1]})")
+                    file_str = file_str[len(str_to_take)-1:]        #discard unidentified token
+                    break
+
+    for key in counters:
+        if counters[key] > 0:
+            errors.append("Unclosed brakets/parentesis found")
+            break
 
     return res_tokens
