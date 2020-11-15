240b5ee2ca3b797a0b6f9438c81a8efa86a1dac0
Павел <pashanoy@gmail.com>
2020-11-15 22:01:07 +0200

    Spaces before parentheses formatters

--- lab1/formatting/formatters.py
+++ lab1/formatting/formatters.py
@@ -1,5 +1,6 @@
 from util.util import *
 
+#Indents formatters
 def apply_indent(tokens, curr_token_index, config):
     """Applies indent specified in config"""
     res = {"before": "", "after": ""}
@@ -29,7 +30,7 @@ def apply_continue_indent(tokens, curr_token_index, config):
 
 def apply_indent_in_php_tags(tokens, curr_token_index, config):
     """Applies indent specified in configs
-    if php tags indent flag is set to true"""
+    if php tags indent flag is set to True"""
     res = {"before": "", "after": ""}
 
     indent = int(config["Indents"]["Indent"])
@@ -40,5 +41,122 @@ def apply_indent_in_php_tags(tokens, curr_token_index, config):
         nesting_level = count_token_nesting(tokens, curr_token_index, ["T_OPEN_TAG", "T_OPEN_TAG_WITH_ECHO"], ["T_CLOSE_TAG"])
 
         res["before"] += " " * indent * nesting_level
+
+    return res
+
+#Spaces before parentheses formatters
+def func_decl_parentheses(tokens, curr_token_index, config):
+    """Puts space between function declaration
+    and paretheses if flag set to True"""
+
+    res = {"before": "", "after": ""}
+
+    if (config["Spaces"]["Function declaration parentheses"] == "True"
+        and check_if_func_declared(tokens, curr_token_index)):
+
+        res["after"] += " "
+
+    return res
+
+def func_call_paretheses(tokens, curr_token_index, config):
+    """Puts space between function call
+    and paretheses if flag set to True"""
+    res = {"before": "", "after": ""}
+
+    if (config["Spaces"]["Function call parentheses"] == "True"
+        and check_if_func_call(tokens, curr_token_index)):
+
+        res["after"] += " "
+
+    return res
+
+def anon_func_paretheses(tokens, curr_token_index, config):
+    """Puts space between anonymous function call
+    and paretheses if flag set to True"""
+
+    res = {"before": "", "after": ""}
+
+    if (config["Spaces"]["Anonymous function parenteses"] == "True"
+        and check_if_token_before_paretheses(tokens, curr_token_index, "T_FUNCTION")):
+
+        res["after"] += " "
+
+    return res
+
+def if_parentheses(tokens, curr_token_index, config):
+    """Puts space between 'if' statement
+    and paretheses if flag set to True"""
+
+    res = {"before": "", "after": ""}
+
+    if (config["Spaces"]["'if' parenteses"] == "True"
+        and check_if_token_before_paretheses(tokens, curr_token_index, "T_IF")):
+
+        res["after"] += " "
+
+    return res
+
+def for_parentheses(tokens, curr_token_index, config):
+    """Puts space between 'for' statement
+    and paretheses if flag set to True"""
+
+    res = {"before": "", "after": ""}
+
+    if (config["Spaces"]["'for' parenteses"] == "True"
+        and check_if_token_before_paretheses(tokens, curr_token_index, "T_FOR")):
+
+        res["after"] += " "
+
+    return res
+
+def while_paretheses(tokens, curr_token_index, config):
+    """Puts space between 'while' statement
+    and paretheses if flag set to True"""
+
+    res = {"before": "", "after": ""}
+
+    if (config["Spaces"]["'while' parenteses"] == "True"
+        and check_if_token_before_paretheses(tokens, curr_token_index, "T_WHILE")):
+
+        res["after"] += " "
+
+    return res
+
+def switch_paretheses(tokens, curr_token_index, config):
+    """Puts space between 'switch' statement
+    and paretheses if flag set to True"""
+
+    res = {"before": "", "after": ""}
+
+    if (config["Spaces"]["'switch' parenteses"] == "True"
+        and check_if_token_before_paretheses(tokens, curr_token_index, "T_SWITCH")):
+
+        res["after"] += " "
+
+    return res
+
+def catch_paretheses(tokens, curr_token_index, config):
+    """Puts space between 'catch' statement
+    and paretheses if flag set to True"""
+
+    res = {"before": "", "after": ""}
+
+    if (config["Spaces"]["'catch' parenteses"] == "True"
+        and check_if_token_before_paretheses(tokens, curr_token_index, "T_CATCH")):
+
+        res["after"] += " "
+
+    return res
+
+def array_init_paretheses(tokens, curr_token_index, config):
+    """Puts space between array initializer
+    and paretheses if flag set to True"""
+
+    res = {"before": "", "after": ""}
+
+    if (config["Spaces"]["Array initializer parentheses"] == "True"
+        and check_if_token_before_paretheses(tokens, curr_token_index, "T_ARRAY")):
+
+        res["after"] += " "
 
     return res
--- lab1/util/util.py
+++ lab1/util/util.py
@@ -20,6 +20,8 @@ def count_token_nesting(tokens, curr_token_index, nesting_token_open, nesting_to
 
 def check_if_first_token_in_line(tokens, curr_token_index):
     """Checks if specified token is placed first in line"""
+    if curr_token_index == 0:
+        return True
 
     curr_token = tokens[curr_token_index]
     prev_token = tokens[curr_token_index-1]
@@ -31,10 +33,64 @@ def check_if_first_token_in_line(tokens, curr_token_index):
 
 def previous_token_is_semicolon(tokens, curr_token_index):
     """Check if last token was a semicolon"""
+    if curr_token_index == 0:
+        return False
 
     prev_token = tokens[curr_token_index-1]
 
     if prev_token.token == "SEMICOLON":
         return True
     
+    return False
+
+def check_if_func_declared(tokens, curr_token_index):
+    """Check if function is declared with current token
+    Function declaration defined as next token sequence:
+    T_FUNCTION, T_STRING, R_PARENTHESES_OPEN"""
+
+    if curr_token_index == 0 or len(tokens) == curr_token_index - 1:
+        return False
+
+    curr_token = tokens[curr_token_index]
+    prev_token = tokens[curr_token_index-1]
+    next_token = tokens[curr_token_index+1]
+
+    if (curr_token == "T_STRING" and prev_token == "T_FUNCTION"
+        and next_token == "R_PARENTHESES_OPEN"):
+
+        return True
+
+    return False
+
+def check_if_func_call(tokens, curr_token_index):
+    """Check if function is called with current token
+    Function call defined as next token sequence:
+    not T_FUNCTION, T_STRING, R_PARENTHESES_OPEN"""
+
+    if curr_token_index == 0 or len(tokens) == curr_token_index - 1:
+        return False
+
+    curr_token = tokens[curr_token_index]
+    next_token = tokens[curr_token_index+1]
+    prev_token = tokens[curr_token_index-1]
+
+    if (curr_token == "T_STRING" and prev_token != "T_FUNCTION"
+        and next_token == "R_PARENTHESES_OPEN"):
+
+        return True
+
+    return False
+
+def check_if_token_before_paretheses(tokens, curr_token_index, needed_token):
+    """Check if current token is needed_token
+    and is followed by paretheses"""
+    if len(tokens) == curr_token_index - 1:
+        return False
+
+    curr_token = tokens[curr_token_index]
+    next_token = tokens[curr_token_index+1]
+
+    if curr_token == needed_token and next_token != "R_PARENTHESES_OPEN":
+        return True
+
     return False
